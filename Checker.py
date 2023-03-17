72964b84d83a2f2d6414b98c7f148d7224b041c4
Timur Zhunussov <z.a.timur@gmail.com>
2023-03-16 20:16:01 -0400

    Testing html report

--- Checker.py
+++ Checker.py
@@ -5,10 +5,10 @@ def unzip_vsix(f):
     # Unzip vsix
     for i in range(len(f)):
         if f[i].endswith('.vsix'):
-            with ZipFile('/Users/zta/Documents/EC521/src/downloads/' + f[i], 'r') as zObject:
+            with ZipFile('/Users/zta/Docs/EC521/EC521/src/downloads/' + f[i], 'r') as zObject:
                 # Extracting all the members of the zip into a specific location.
                 print(f[i], i)
-                zObject.extractall(path='/Users/zta/Documents/EC521/src/unzipped/'+f[i][:-5])
+                zObject.extractall(path='/Users/zta/Docs/EC521/EC521/src/unzipped/'+f[i][:-5])
         else:
             continue
 
@@ -22,28 +22,28 @@ def semgrep_check(d):
 
 
 def snyk_check(d):
-    # Check with Semgrep
+    # Check with Snyk
     
     for j in range(len(d)):
-        # cmd = 'snyk code test ' + check_dir + d[j] + ' --sarif-file-output=/Users/zta/Documents/EC521/src/scan_results_snyk/'+ d[j] +'.json'
-        cmd = 'snyk test ' + check_dir + d[j] + ' --all-projects >> '+ '/Users/zta/Documents/EC521/src/scan_results_snyk/' + d[j]+'.txt'
+        # cmd = 'snyk code test ' + check_dir + d[j] + ' --sarif-file-output=/Users/zta/Docs/EC521/EC521/src/scan_results_snyk/'+ d[j] +'.json'
+        cmd = 'snyk test ' + check_dir + d[j] + ' --all-projects >> '+ '/Users/zta/Docs/EC521/EC521/src/scan_results_snyk/' + d[j]+'.txt'
         print(cmd)
         os.system(cmd)
 
 # Walk on folders d = floders
-check_dir = '/Users/zta/Documents/EC521/src/unzipped/'
+check_dir = '/Users/zta/Docs/EC521/EC521/src/unzipped/'
 d = []
 d = next(os.walk(check_dir))[1]
 
-parent_dir = '/Users/zta/Documents/EC521/src/downloads'
+parent_dir = '/Users/zta/Docs/EC521/EC521/src/downloads'
 # Walk on folders f = files
 f = []
 f = next(os.walk(parent_dir))[2]
 
-report_dir = '/Users/zta/Documents/EC521/src/scan_results/'
+report_dir = '/Users/zta/Docs/EC521/EC521/src/scan_results_semgrep/'
 
 # Function calls
-unzip_vsix(f)
-semgrep_check(d)
+# unzip_vsix(f)
+# semgrep_check(d)
 snyk_check(d)
 
--- combine.py
+++ combine.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import os
+from IPython.display import HTML
+
 df = pd.DataFrame()
 
 # Parses Snyk results
-parent_dir = '/Users/zta/Documents/EC521/src/scan_results_snyk/'
+parent_dir = '/Users/zta/Docs/EC521/EC521/src/scan_results_snyk/'
 # Walk on folders f = files
 f = []
 f = next(os.walk(parent_dir))[2]
@@ -24,7 +26,7 @@ df1 = pd.DataFrame(result1)
 
 
 # Parses Semgrep results
-parent_dir = '/Users/zta/Documents/EC521/src/scan_results/'
+parent_dir = '/Users/zta/Docs/EC521/EC521/src/scan_results_semgrep/'
 # Walk on folders f = files
 f = []
 f = next(os.walk(parent_dir))[2]
@@ -35,17 +37,27 @@ for j in range(len(f)):
         if f[j].endswith('.txt'):
             for ln in fi:
                 if ln.__contains__("failures="):
-                    result2.append([f[j],ln])
+                    result2.append([f[j],ln[35:48]])
                     break
 
 
  
 df2 = pd.DataFrame(result2)
-print(df2)
+# print(df2)
 
 # Merges results
 df3 = df2.merge(df1, how='outer', on=0)
-df3 = df3.rename(columns={0: 'Name', '1_x': 'SemGrep_Failures', '1_y': 'SNYK_Issues'})
+df3 = df3.rename(columns={0: 'Name', '1_x': 'SemGrep_Issues', '1_y': 'SNYK_Issues'})
+df3['SNYK_Issues'] = df3['SNYK_Issues'].fillna(0)
+
+
+df3['SNYK file'] = '<a href="./src/scan_results_snyk/' + df3['Name'] + '">' + df3['Name'] + '</a>'
+
+
+df3.insert(loc = 2, column = 'Semgrep file', value = '<a href="./src/scan_results_semgrep/' + df3['Name'] + '">' + df3['Name'] + '</a>')
+
+
+df3.to_html('report.html', render_links=True, escape=False)
 
 df3.to_csv('file1.csv')
 print(df3)
