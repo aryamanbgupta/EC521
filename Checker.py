efd978d6ef81aa92b6bfc6d8ad1c7ebe7d1b5de4
Timur Zhunussov <z.a.timur@gmail.com>
2023-04-30 23:01:06 -0400

    Fixed varaiables for README

--- Checker.py
+++ Checker.py
@@ -14,47 +14,47 @@ def unzip_vsix(files, download_dir, unzipped_dir):
             continue
 
 
-def semgrep_check(directories, check_dir, report_dir):
+def semgrep_check(directories, CHECK_DIR, report_dir):
     for dir in directories:
-        # cmd = f'semgrep --config=auto {os.path.join(check_dir, dir)} --output {os.path.join(report_dir, dir)}.json --json'
-        # os.chdir(os.path.join(check_dir, dir))
-        cmd = f'semgrep --config=auto {os.path.join(check_dir, dir)} --junit-xml >> {os.path.join(report_dir, dir)}.txt'
+        # cmd = f'semgrep --config=auto {os.path.join(CHECK_DIR, dir)} --output {os.path.join(report_dir, dir)}.json --json'
+        # os.chdir(os.path.join(CHECK_DIR, dir))
+        cmd = f'semgrep --config=auto {os.path.join(CHECK_DIR, dir)} --junit-xml >> {os.path.join(report_dir, dir)}.txt'
 
         print(cmd)
         os.system(cmd)
 
 
-def snyk_check(directories, check_dir, report_dir):
+def snyk_check(directories, CHECK_DIR, report_dir):
     for dir in directories:
-        cmd = f'snyk test {os.path.join(check_dir, dir)} --all-projects --json-file-output={os.path.join(report_dir, dir)}.txt'
-        # cmd = f'snyk test {os.path.join(check_dir, dir)} --all-projects >> {os.path.join(report_dir, dir)}.txt'
+        cmd = f'snyk test {os.path.join(CHECK_DIR, dir)} --all-projects --json-file-output={os.path.join(report_dir, dir)}.txt'
+        # cmd = f'snyk test {os.path.join(CHECK_DIR, dir)} --all-projects >> {os.path.join(report_dir, dir)}.txt'
         print(cmd)
         os.system(cmd)
 
 
 # Searches json files
-def json_search(report_semgrep_dir,report_snyk_dir, keyword = 'path traversal'):
+def json_search(REPORT_SEMGREP_DIR,REPORT_SNYK_DIR, KEYWORD = 'path traversal'):
     data1 = []
-    for filename in os.listdir(report_semgrep_dir):
+    for filename in os.listdir(REPORT_SEMGREP_DIR):
         if filename.endswith('.json'):
-            file_path = os.path.join(report_semgrep_dir, filename)
+            file_path = os.path.join(REPORT_SEMGREP_DIR, filename)
             with open(file_path, 'r') as f:
                 json_data = json.load(f)
             json_str = json.dumps(json_data)
-            match = bool(re.search(keyword, json_str))
-            data1.append({'filename': filename, 'Semgrep contains_keyword': match})
+            match = bool(re.search(KEYWORD, json_str))
+            data1.append({'filename': filename, 'Semgrep contains_KEYWORD': match})
 
     df1 = pd.DataFrame(data1)
  
     data2 = []
-    for filename in os.listdir(report_snyk_dir):
+    for filename in os.listdir(REPORT_SNYK_DIR):
         if filename.endswith('.json'):
-            file_path = os.path.join(report_snyk_dir, filename)
+            file_path = os.path.join(REPORT_SNYK_DIR, filename)
             with open(file_path, 'r') as f:
                 json_data = json.load(f)
             json_str = json.dumps(json_data)
-            match = bool(re.search(keyword, json_str))
-            data2.append({'filename': filename, 'Snyk contains_keyword': match})
+            match = bool(re.search(KEYWORD, json_str))
+            data2.append({'filename': filename, 'Snyk contains_KEYWORD': match})
             print(data2)
     df2 = pd.DataFrame(data2)
     
@@ -68,24 +68,24 @@ def json_search(report_semgrep_dir,report_snyk_dir, keyword = 'path traversal'):
 
 
 # Searches txt files
-def txt_search(report_semgrep_dir,report_snyk_dir, keyword = 'path traversal'):
+def txt_search(REPORT_SEMGREP_DIR,REPORT_SNYK_DIR, KEYWORD = 'path traversal'):
     data1 = []
-    for filename in os.listdir(report_semgrep_dir):
+    for filename in os.listdir(REPORT_SEMGREP_DIR):
         if filename.endswith('.txt'):
-            file_path = os.path.join(report_semgrep_dir, filename)
+            file_path = os.path.join(REPORT_SEMGREP_DIR, filename)
             with open(file_path, 'r') as f:
                 txt_data = f.read()
-            match = bool(re.search(keyword, txt_data))
+            match = bool(re.search(KEYWORD, txt_data))
             data1.append({'filename': filename, 'Semgrep PathTraversal': match})
     df1 = pd.DataFrame(data1)
     
     data2 = []
-    for filename in os.listdir(report_snyk_dir):
+    for filename in os.listdir(REPORT_SNYK_DIR):
         if filename.endswith('.txt'):
-            file_path = os.path.join(report_snyk_dir, filename)
+            file_path = os.path.join(REPORT_SNYK_DIR, filename)
             with open(file_path, 'r') as f:
                 txt_data = f.read()
-            match = bool(re.search(keyword, txt_data))
+            match = bool(re.search(KEYWORD, txt_data))
             data2.append({'filename': filename, 'Snyk PathTraversal': match})
 
     df2 = pd.DataFrame(data2)
@@ -106,25 +106,25 @@ def txt_search(report_semgrep_dir,report_snyk_dir, keyword = 'path traversal'):
 
 def main():
     # Folder for downloaded extensions
-    parent_dir = '/Users/zta/Docs/EC521/EC521/src/downloads'
+    PARENT_DIR = '/Users/zta/Docs/EC521/EC521/src/downloads'
     # Folder for unzipped extensions
-    check_dir = '/Users/zta/Docs/EC521/EC521/src/unzipped_200/'
+    CHECK_DIR = '/Users/zta/Docs/EC521/EC521/src/unzipped_200/'
     # Folders to put reports
-    report_semgrep_dir = '/Users/zta/Docs/EC521/EC521/src/scan_results_semgrep_200/'
-    report_snyk_dir = '/Users/zta/Docs/EC521/EC521/src/scan_results_snyk_200_json/'
+    REPORT_SEMGREP_DIR = '/Users/zta/Docs/EC521/EC521/src/scan_results_semgrep_200/'
+    REPORT_SNYK_DIR = '/Users/zta/Docs/EC521/EC521/src/scan_results_snyk_200_json/'
 
     # Get files and directories
-    files = next(os.walk(parent_dir))[2]
-    directories = next(os.walk(check_dir))[1]
+    files = next(os.walk(PARENT_DIR))[2]
+    directories = next(os.walk(CHECK_DIR))[1]
 
     # Function calls:
-    # unzip_vsix(files, parent_dir, check_dir)
-    # semgrep_check(directories, check_dir, report_semgrep_dir)
-    # snyk_check(directories, check_dir, report_snyk_dir)
-    # json_search(report_semgrep_dir,report_snyk_dir)
-    # txt_search(report_semgrep_dir,report_snyk_dir, keyword = 'Directory')
-    txt_search(report_semgrep_dir,report_snyk_dir)
-    # json_search(report_semgrep_dir,report_snyk_dir, keyword = 'slip')
+    unzip_vsix(files, PARENT_DIR, CHECK_DIR)
+    semgrep_check(directories, CHECK_DIR, REPORT_SEMGREP_DIR)
+    snyk_check(directories, CHECK_DIR, REPORT_SNYK_DIR)
+    # json_search(REPORT_SEMGREP_DIR,REPORT_SNYK_DIR)
+    # txt_search(REPORT_SEMGREP_DIR,REPORT_SNYK_DIR, KEYWORD = 'Directory')
+    txt_search(REPORT_SEMGREP_DIR,REPORT_SNYK_DIR)
+    # json_search(REPORT_SEMGREP_DIR,REPORT_SNYK_DIR, KEYWORD = 'slip')
 
 if __name__ == "__main__":
     main()
